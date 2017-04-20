1013f26afffcf1dbfff1e85423e5dafea33946a0
mdiannna <mdiannna@yahoo.com>
2017-04-20 23:50:01 +0300

    added class Postare

--- Project/postari.h
+++ Project/postari.h
@@ -1,6 +1,20 @@
 #ifndef POSTARI_H_INCLUDED
 #define POSTARI_H_INCLUDED
 
+#include <iostream>
+using namespace std;
+
+class Postare{
+    char * text;
+    //Persoana persoana;
+    enum nivelDeIntimitate{
+        toti,
+        prieteni,
+        followeri
+    };
+public:
+    void postManager();
+};
 
 
 #endif // POSTARI_H_INCLUDED
