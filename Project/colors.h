c70486b11d20b7ffc8a5456f43ff4437895f1e37
mdiannna <mdiannna@yahoo.com>
2017-05-10 21:19:51 +0300

    added little documentation to colors.h

--- Project/colors.h
+++ Project/colors.h
@@ -1,5 +1,16 @@
+/*****************************
+Library for printing colored text in console
+******************************
+WARNING: The library will work on most UNIX systems and
+	is not supported in Win32 console component of Microsoft Windows 
+	before Windows 10 update TH2
+more information here: https://en.wikipedia.org/wiki/ANSI_escape_code
+*****************************/
+
 #ifndef COLORS_H_INCLUDED
 #define COLORS_H_INCLUDED
+
+
 
 char red[] = { 0x1b, '[', '1', ';', '3', '1', 'm', 0 };
 char green[] = { 0x1b, '[', '1', ';', '3', '2', 'm', 0 };
@@ -7,5 +18,6 @@ char yellow[] = { 0x1b, '[', '1', ';', '3', '3', 'm', 0 };
 char blue[] = { 0x1b, '[', '1', ';', '3', '4', 'm', 0 };
 char magenta[] = { 0x1b, '[', '1', ';', '3', '5', 'm', 0 };
 char cyan[] = { 0x1b, '[', '1', ';', '3', '6', 'm', 0 };
+
 
 #endif // COLORS_H_INCLUDED
