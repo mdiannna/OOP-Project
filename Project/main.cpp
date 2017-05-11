fdd690ba416890f0e759c17d0c4f2859416ea801
dorinacraciun <dorinaacraciun@gmail.com>
2017-05-11 16:33:48 +0300

    << comentariu

--- Project/main.cpp
+++ Project/main.cpp
@@ -8,18 +8,11 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    //sandbox();
-//    test_util();
-   // testPostare();
-=======
-    // sandbox();
-	// test_util();
+
+    sandbox();
+    test_util();
 	testConsole();
  	testPostare();
->>>>>>> 0b723bc70536a507a9c4de4dd8e521a666e373ce
-
-    cout << "\n";
     test_comentarii();
     return 0;
 }
